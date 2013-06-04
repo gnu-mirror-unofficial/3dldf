8cbcb132cbe39dab9886505c2f63e6f40c9c8230
Laurence Finston <Laurence.Finston@gmx.de>
2013-06-04 13:52:35 +0200

    Added explanatory comment.

--- rebuild.sh
+++ rebuild.sh
@@ -5,6 +5,15 @@
 
 #### Created by Laurence D. Finston (LDF) Mon Jun  3 20:42:11 CEST 2013
 
+#### Usage:  rebuild.sh [ARG] [ARG]
+#### Optional arguments:  1:  Run 'make maintainer-clean'
+####                      2:  Run 'make'
+####
+#### It doesn't matter what arguments are used, as long as they exist. 
+#### That is, they aren't checked.  '1' (the numeral 1) is a good choice.
+####
+#### LDF 2013.06.04.
+
 cd /home/lfinsto/3dldf
 
 #echo "Number of args:  $#"
@@ -16,6 +25,7 @@ fi
 
 libtoolize && aclocal && autoconf && autoheader && automake \
    && configure --prefix=`pwd` LIBS="-lgsl -lgslcblas -lm"
+
 r=$?
 
 if test $r -eq 0 -a $# -gt 1
