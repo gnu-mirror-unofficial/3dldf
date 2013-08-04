790b62a09b988aa9f9df3d646185a559a58ff7bb
Laurence Finston <lfinsto@gwdg.de>
2013-08-04 18:59:36 +0200

    Initial version.

---
+++ reconfig.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#### reconfig.sh
+#### /home/lfinsto/3dldf_savannah/master/reconfig.sh
+
+#### Created by Laurence D. Finston (LDF) Sun Aug  4 18:59:09 CEST 2013
+
+./configure --prefix=/home/lfinsto/3dldf_local LIBS="-lgsl -lgslcblas -lm"
+
+exit 0
