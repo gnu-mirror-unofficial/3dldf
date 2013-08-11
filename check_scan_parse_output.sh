65e008087bbe79319821f190bb4c66a58bd72a38
Laurence Finston <lfinsto@gwdg.de>
2013-08-11 18:32:43 +0200

    Added.

---
+++ check_scan_parse_output.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+#### check_scan_parse_output.sh
+#### [...]/Finston/gwrdifpk/src/check_scan_parse_output.sh
+
+#### Created by Laurence D. Finston (LDF) Sun May 19 16:48:34 CEST 2013
+
+#echo "Arg 1 == $1"
+#echo "Arg 2 == $2"
+
+if test -e "$2"
+then
+   echo "$2 exists"
+   diff --brief --ignore-all-space --ignore-blank-lines \
+        --ignore-matching-lines='^[[:space:]]*\(#line\|/\*\)' $1 $2 
+   r=$?
+   if test $r -eq 1
+   then
+      echo "$2 has changed."
+      mv $1 $2
+   else
+      echo "$2 is unchanged."
+   fi
+else
+   echo "$2 does not exist"
+   mv $1 $2
+fi 
+
+exit 0
