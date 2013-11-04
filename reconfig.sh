b4b037ff702bb7cb76c195cf4f7a2dddef42ae46
Laurence Finston <Laurence.Finston@gwdg.de>
2013-11-04 14:16:29 +0100

    Minor changes.

--- reconfig.sh
+++ reconfig.sh
@@ -5,17 +5,23 @@
 
 #### Created by Laurence D. Finston (LDF) Sun Aug  4 18:59:09 CEST 2013
 
+#### Usage:  reconfig.sh [ARG_0 [ARG_1]]
+####
+#### Arguments:
+####             ARG_0:  If 0, build shared libraries.  Otherwise, don't build shared libraries.
+####             ARG_1:  If present, call 'make' after 'configure'.
+
 if test $# -gt 0 
 then
    if test $1 -ne 0 
    then
-      echo "Not building  shared libraries"
+      echo "Not building shared libraries"
       CONF_STATIC="--disable-shared " 
    else
-      echo "Building  shared libraries"
+      echo "Building shared libraries"
    fi
    else
-      echo "Building  shared libraries"
+      echo "Building shared libraries"
 fi
 
 configure --prefix=`pwd` $CONF_STATIC LIBS="-lgsl -lgslcblas -lm"
@@ -27,25 +33,20 @@ then
 
    if test $1 -ne 0 
    then
-      echo "Did not build  shared libraries"
+      echo "Did not build shared libraries"
    else
-      echo "Built  shared libraries"
+      echo "Built shared libraries"
    fi
-fi
-
-echo
-
-if test $# -gt 0 
+elif test $# -gt 0 
 then
    if test $1 -ne 0 
    then
       echo "Not building  shared libraries"
    else
-      echo "Building  shared libraries"
+      echo "Building shared libraries"
    fi
    else
-      echo "Building  shared libraries"
+      echo "Building shared libraries"
 fi
-
 
 exit 0
