aa5a3c2a4153066b1da15b9cd20c06d17ed6209e
Laurence Finston <Laurence.Finston@gmx.de>
2021-03-21 21:23:42 +0100

    Updated copyright notices.

--- src/ttemp.cxx
+++ src/ttemp.cxx
@@ -1,181 +1,258 @@
-/* songlist.cxx  */
-/* Created by Laurence D. Finston 10.2017  */
+/* ttemp.cxx  */
+/* Created by Laurence D. Finston Sun 21 Mar 2021 05:00:23 PM CET  */
 
 #include <stdlib.h>
 #include <stdio.h>
-
-#include <string.h>
+#include <errno.h>
+#include <limits.h>
+#include <math.h>
 
 #include <sys/types.h>
 
-#include <errno.h>
-
-#include <string>
+#include <algorithm>
+#include <bitset>
+#include <exception>
+#include <float.h>
 #include <iomanip>
 #include <ios>
 #include <iostream>
-#include <stdarg.h>
-#include <limits.h>
-#include <algorithm>
-#include <fstream>
 #include <iterator>
-#include <math.h>
-#include <sstream> 
-#include <time.h> 
-#include <vector> 
-#include <map>
-#include <cfloat>
+#include <limits>
+#include <new>
+#include <sstream>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <valarray>
+#include <vector>
+
+
 
 /* ** (2) |main| definition  */
 
 using namespace std;
 
+typedef unsigned int i_type;
+
 int
-main(int argc, char **argv)
+main(void)
 {
-/* *** (3)   */
 
-
-  cout.precision(26);
-#if 0 
-  cout << "FLT_MAX == " << FLT_MAX << endl;
-
-  cout << "FLT_MAX_EXP == " << FLT_MAX_EXP << endl;
-
-
-
-
-  int i = 0;
-
-  float g = f - 1;
-#endif 
-
-  float f = FLT_MAX;
-
-  3.402823466385288598117042e+38
-
-  g = 3.402823466385000000000000e+38
-
-    cout << "f == " << f
-	 << "g == " << g;
-    
-  printf("(g < f): %d\n", (g < f));
-    
-  float g = f;
-
-  stringstream temp_strm;
-
-  string s;
-
-  string int_str;
-
-  string frac_str;
-
-  string exp_str;
-
-  temp_strm.precision(25);
+  cout.precision(100);
   
-  temp_strm << f;
+  bool verbose = true;
+  bool DEBUG   = true;
+
+  float *rp = 0;
   
-  cout << "temp_strm.str() == " << temp_strm.str() << endl;
+  unsigned int UINT_SIZE = sizeof(unsigned int);
+  unsigned int ULONG_SIZE = sizeof(unsigned long);
+  unsigned int ULONG_LONG_SIZE = sizeof(unsigned long long);
 
-  int_str = temp_strm.str();
+  unsigned int FLT_SIZE = sizeof(float);
+  unsigned int DBL_SIZE = sizeof(double);
+  unsigned int LONG_DBL_SIZE = sizeof(long double);
 
-  frac_str = int_str;
-  
-  int pos;
-  
-  pos = int_str.find_first_of(".");
-
-  int_str.erase(pos);
-
-  frac_str.erase(0, pos + 1);
-  
-  exp_str = frac_str;
-
-  pos = frac_str.find_first_of("e");
-
-  frac_str.erase(pos);
-
-  exp_str.erase(0, pos + 2);
-  
-  cout << "int_str  == " << int_str << endl
-       << "frac_str == " << frac_str << endl
-       << "exp_str  == " << exp_str << endl;
-
-  float ff;
-
-  s = frac_str;
-
-  int i;
-
-  string temp_str;
-  
-  for (pos = (s.length() - 1); pos >= 0; pos--)
+  if (verbose)
     {
-
-      printf("pos: %d\n", pos);
-
-      printf("(pos > 0): %d\n", (pos > 0));
-      printf("(pos < 0): %d\n", (pos < 0));
-      printf("(pos == 0): %d\n", (pos == 0));
-
-      temp_strm.str("");
-      
-      cout << "frac_str == " << frac_str << endl
-	   << "s[" << pos << "] == " << s[pos] << endl;
-
-      temp_str = s[pos];
-      
-      try
-	{
-	  i = stoi(temp_str);
-	}
-      catch (const out_of_range& oor)
-	{
-	  cerr << "Out of Range error: " << oor.what() << '\n';
-	}
-      catch (const std::invalid_argument& ia)
-	{
-	  cerr << "Invalid argument: " << ia.what() << '\n';
-	}
-
+      cout << "INT_WIDTH       == " << INT_WIDTH  << endl    
+           << "LONG_WIDTH      == " << LONG_WIDTH  << endl          
+           << "UINT_SIZE       == " << UINT_SIZE  << endl
+           << "ULONG_SIZE      == " << ULONG_SIZE << endl
+           << "ULONG_LONG_SIZE == " << ULONG_LONG_SIZE << endl
+           << "FLT_SIZE        == " << FLT_SIZE << endl
+	   << "FLT_MAX         == " << FLT_MAX << endl
+           << "DBL_MAX         == " << DBL_MAX << endl
+           << "DBL_SIZE        == " << DBL_SIZE << endl
+           << "LONG_DBL_SIZE   == " << LONG_DBL_SIZE  << endl
+           << "CHAR_BIT        == " << CHAR_BIT << endl
+           << "FLT_MANT_DIG    == " << FLT_MANT_DIG << endl
+           << "FLT_DIG         == " << FLT_DIG << endl
+           << "FLT_MIN_10_EXP  == " << FLT_MIN_10_EXP << endl
+           << "FLT_MAX_10_EXP  == " << FLT_MAX_10_EXP << endl
+           << "FLT_RADIX       == " << FLT_RADIX << endl;
     }
 
+
+  i_type bit_pattern_i_type;
+  i_type sign_bits;
+  i_type result;
+  bitset<sizeof(i_type) * CHAR_BIT> b;
+  bitset<sizeof(unsigned long) * CHAR_BIT> bb;
+   
+
+  float second_largest_float = 0.0F;
+
+  float f = FLT_MAX;
+  double d = DBL_MAX;
+  long double l = LDBL_MAX;
+
+  i_type *ip = reinterpret_cast<i_type *>(&f);
+
+  unsigned long *ulp = reinterpret_cast<unsigned long *>(&d);
+
+  unsigned long *ulp = reinterpret_cast<unsigned long *>(&d);
+  
+  b = *ip;
+  
+  if (verbose)
+    cout << "FLT_MAX == " << FLT_MAX << " == " << b << endl;
+
+  bb = *ulp;
+  
+  cout << "DBL_MAX == " << DBL_MAX << " == " << bb << endl;
+  
   cerr << "Enter <RETURN> to continue: ";
-  getchar();       
+  getchar(); 
+
+  // For double:  sign_bits shift:
+  // 52
+  // 63  
+
+  
+  bit_pattern_i_type = 1;
+  
+  b = bit_pattern_i_type;
+
+  if (verbose)
+    cout << "b (bit_pattern_i_type) == " << b << endl;
    
+  result = bit_pattern_i_type ^ *ip;
+  b = result;
 
+  if (verbose)
+    cout << "result == " << result << " == " << b << endl;
 
+  rp = reinterpret_cast<float *>(&result);
 
-#if 0 
-      try
+  int counter;
+
+  cout << "Before loop." << endl;
+  
+  sign_bits = (1 << 23) & (1 << 31);
+
+  b = sign_bits;
+  
+  cout << "sign_bits == " << b << endl;
+
+  for (int i = 0; i < (sizeof(float) * CHAR_BIT); ++i)
+    {
+      if (verbose)
+        cout << "i == " << i << endl;
+
+      b = *reinterpret_cast<i_type *>(&second_largest_float);
+      
+      cout << "second_largest_float == " << second_largest_float << " == " << b
+	   << endl;
+
+      if (second_largest_float > 0)
 	{
-	  ff = stof(temp_str);
+	  cerr << "Enter <RETURN> to continue: ";
+	  getchar();
 	}
-      catch (const out_of_range& oor)
+      
+      bit_pattern_i_type = 1;
+      bit_pattern_i_type <<= i;
+
+      b = bit_pattern_i_type;
+
+      if (verbose)
+	cout << "After shift and XOR:" << endl
+	     << "bit_pattern_i_type (1 << " << i << ") == " 
+	     << bit_pattern_i_type << endl
+	     <<	" b (bit_pattern_i_type) == " << b << endl; 
+
+      result = (bit_pattern_i_type ^ *ip) ^ sign_bits;
+
+      b = result;
+      
+      if (verbose)
+	cout << "result == " << result << " == " << b << endl;
+
+      rp = reinterpret_cast<float *>(&result);
+
+      if (verbose)
+	cout << "*rp (result as float) == " << *rp << endl;
+
+      if (isnan(*rp))
 	{
-	  cerr << "Out of Range error: " << oor.what() << '\n';
+	  if (verbose)
+	    cout << "*rp is nan.\nContinuing.\n\n";
 	}
-      catch (const std::invalid_argument& ia)
+      else if (*rp < 0)
 	{
-	  cerr << "Invalid argument: " << ia.what() << '\n';
+	  if (verbose)
+	    cout << "*rp < 0.\nContinuing.\n\n";
+	}
+      else if (*rp == 0)
+	{
+	  if (verbose)
+	    cout << "*rp == 0.\nContinuing.\n\n";
 	}
 
-      cout << "ff  == " << ff << endl;
-#endif 
+      else if (*rp > FLT_MAX) // This can't happen
+	{
+	  if (verbose)
+	    cout << "*rp >  FLT_MAX" << endl << "Continuing."
+		 << endl;
+	}
 
+      else if (*rp == FLT_MAX)
+	{
+	  if (verbose)
+	    cout << "*rp ==  FLT_MAX" << endl << "Continuing."
+		 << endl;
+	  cerr << "Enter <RETURN> to continue: ";
+	  getchar(); 
+	}
 
-#if 0 
-  cout << "g == " << g << endl;
+      else if (second_largest_float > *rp)
+	{
+	  if (verbose)
+	    cout << "second_largest_float > *rp\nContinuing." 
+		 << "\n";
+	} 
 
-  cout << "g < f is " << (g < f) << endl;
+      else if (second_largest_float == *rp)
+	{
+	  if (verbose)
+	    cout << "second_largest_float == *rp\nContinuing." 
+		 << "\n";
+	} 
 
-#endif 
+      else if (*rp > second_largest_float && *rp < FLT_MAX)
+	{
+	  if (verbose)
+	    cout << "*rp > second_largest_float && "
+		 << "*rp < FLT_MAX" << endl
+		 << "Setting second_largest_float to *rp"
+		 << " and counter to i (" << i << ").\n";
+
+	  cerr << "Enter <RETURN> to continue: ";
+	  getchar(); 
+
+	  second_largest_float = *rp;
+	  counter = i;
+	}
+      else 
+	{
+	  if (verbose)
+	    cout << "Some other condition. Continuing.\n";
+	  cerr << "Enter <RETURN> to continue: ";
+	  getchar(); 
+	}
+	      
+    } /* |for| */
+
+  cout << "After loop." << endl
+       << "counter              == " << counter << endl
+       << "second_largest_float == " << second_largest_float << endl;
 
   
   
-   
+  cout << "Exiting `ttemp' successfully with exit status 0." << endl;
+
+  exit(0);
 
 }  /* End of |main| definition  */
 
