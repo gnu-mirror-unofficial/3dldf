2a05460b3e459f1ac62836172105d92fd79ef7f1
Laurence Finston <Laurence.Finston@gmx.de>
2021-04-05 23:19:06 +0200

    Edited.

--- src/create_headers.sh
+++ src/create_headers.sh
@@ -1,3 +1,61 @@
 #! /bin/bash
 
-make arc.h++  circles.h++ cncsctlt.h++ cnepspng.h++  colors.h++ complex.h++ cones.h++ conicsct.h++ conveps.h++ crclslc.h++ creatnew.h++ cuboid.h++ curves.h++  cylinder.h++ dashptrn.h++ ddchdrn.h++ deftfncs.h++ ellipses.h++ ellpsoid.h++ ellpsslc.h++ figures.h++ glyphs.h++ gsltmplt.h++ helices.h++ hyprbola.h++ imetfncs.h++ io.h++ lines.h++ loader.h++ main.h++ matrices.h++    nurbs.h++ origami.h++ parabola.h++ parabold.h++ paraellp.h++ parahypr.h++ parser2.h++   paths.h++ patterns.h++                 pctfncs0.h++            pens.h++             pictures.h++  planes.h++    plyhdslc.h++     pntrvcf0.h++ pntrvctr.h++  points.h++ polygons.h++ polyhed.h++                          predctes.h++ primes.h++   prrfnc0.h++   pspglb.h++               rectangs.h++  rhtchdrn.h++ scanner2.h++ scanprse.h++ scanprsf.h++ scan.h++ sctpcrt.h++ sctpfncs.h++   shapes.h++ solfaced.h++ solids.h++   spheres.h++ sphrdevl.h++ transfor.h++ triangle.h++  utility.h++ 
+#### * Copyright and License.
+
+#### This file is part of GNU 3DLDF, a package for three-dimensional drawing. 
+#### Copyright (C) 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021 The Free Software Foundation, Inc.
+
+#### GNU 3DLDF is free software; you can redistribute it and/or modify 
+#### it under the terms of the GNU General Public License as published by 
+#### the Free Software Foundation; either version 3 of the License, or 
+#### (at your option) any later version. 
+
+#### GNU 3DLDF is distributed in the hope that it will be useful, 
+#### but WITHOUT ANY WARRANTY; without even the implied warranty of 
+#### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+#### GNU General Public License for more details. 
+
+#### You should have received a copy of the GNU General Public License 
+#### along with GNU 3DLDF; if not, write to the Free Software 
+#### Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA 
+
+#### GNU 3DLDF is a GNU package.  
+#### It is part of the GNU Project of the  
+#### Free Software Foundation 
+#### and is published under the GNU General Public License. 
+#### See the website http://www.gnu.org 
+#### for more information.   
+#### GNU 3DLDF is available for downloading from 
+#### http://www.gnu.org/software/3dldf/LDF.html. 
+ 
+#### Please send bug reports to Laurence.Finston@gmx.de 
+#### The mailing list help-3dldf@gnu.org is available for people to 
+#### ask other users for help.  
+#### The mailing list info-3dldf@gnu.org is for sending 
+#### announcements to users. To subscribe to these mailing lists, send an 
+#### email with ``subscribe <email-address>'' as the subject.  
+
+#### The author can be contacted at: 
+
+#### Laurence D. Finston                  
+#### c/o Free Software Foundation, Inc.  
+#### 51 Franklin St, Fifth Floor          
+#### Boston, MA  02110-1301              
+#### USA                                 
+
+#### Laurence.Finston@gmx.de 
+
+make arc.h++ circles.h++ cncsctlt.h++ cnepspng.h++ colors.h++ \
+     complex.h++ cones.h++ conicsct.h++ conveps.h++ crclslc.h++ \
+     creatnew.h++ cuboid.h++ curves.h++ cylinder.h++ dashptrn.h++ \
+     ddchdrn.h++ deftfncs.h++ ellipses.h++ ellpsoid.h++ ellpsslc.h++ \
+     figures.h++ glyphs.h++ gsltmplt.h++ helices.h++ hyprbola.h++ \
+     imetfncs.h++ io.h++ lines.h++ loader.h++ main.h++ matrices.h++ \
+     nurbs.h++ origami.h++ parabola.h++ parabold.h++ paraellp.h++ \
+     parahypr.h++ parser2.h++ paths.h++ patterns.h++ pctfncs0.h++ pens.h++ \
+     pictures.h++ planes.h++ plyhdslc.h++ pntrvcf0.h++ pntrvctr.h++ \
+     points.h++ polygons.h++ polyhed.h++ predctes.h++ primes.h++ \
+     prrfnc0.h++ pspglb.h++ rectangs.h++ rhtchdrn.h++ scanner2.h++ \
+     scanprse.h++ scanprsf.h++ scan.h++ sctpcrt.h++ sctpfncs.h++ shapes.h++ \
+     solfaced.h++ solids.h++ spheres.h++ sphrdevl.h++ transfor.h++ \
+     triangle.h++ utility.h++
