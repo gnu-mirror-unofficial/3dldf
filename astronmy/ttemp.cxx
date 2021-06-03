fcc8148f27d489fa763d444a8b76a5fb68e4daa3
Laurence Finston <Laurence.Finston@gmx.de>
2021-06-03 13:11:59 +0200

    Added.

---
+++ astronmy/ttemp.cxx
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <cstdio>
+#include <float.h>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <iterator>
+#include <math.h>
+#include <new>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <mysql.h>
+
+using namespace std;
+
+int
+main(int argc, char *arv[])
+{
+   printf("Entering `main'.\n");
+  
+   return 0;
+}
