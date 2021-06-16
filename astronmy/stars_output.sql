0cabf996492344c8edb1a7d3cfc232f7f285150d
Laurence Finston <Laurence.Finston@gmx.de>
2021-06-16 16:43:08 +0200

    Added.

---
+++ astronmy/stars_output.sql
@@ -0,0 +1,45 @@
+/* stars_output.sql */
+
+/* !! START HERE:  LDF 2021.06.16.  Find these stars and add database entries.  */ 
+
+/* ** (2) */
+
+149 Gamma Trianguli Australis
+
+/* ** (2) */
+
+179 Omicron^2 Canis Majoris
+
+/* ** (2) */
+
+199 N Velorum
+
+/* ** (2) */
+
+210 G Scorpii
+
+/* ** (2) */
+
+231 p Carinae
+
+https://en.wikipedia.org/wiki/PP_Carinae
+p Carinae
+Carina constellation map.svg
+Red circle.svg
+Location of p Carinae (circled)
+Observation data
+Epoch J2000.0      Equinox J2000.0
+Constellation 	Carina
+Right ascension 	10h 32m 01.46297s[1]
+Declination 	–61° 41′ 07.1963″[1]
+Apparent magnitude (V) 	3.22 - 3.55[2]
+
+/* ** (2) */
+
+254 q Carinae
+
+/* ** (2) */
+
+268 a Carinae
+
+/* ** (2) */
