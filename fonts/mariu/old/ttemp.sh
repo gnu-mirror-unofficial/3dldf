5ba989759eca0ac23ad369cc414c8f9a711aed4e
Laurence Finston <Laurence.Finston@gmx.de>
2022-01-13 11:05:12 +0100

    Added.

---
+++ fonts/mariu/old/ttemp.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#### ttemp.sh
+#### Created by Laurence D. Finston (LDF) Thu 13 Jan 2022 11:04:06 AM CET
+
+#### !! START HERE:  LDF 2022.01.13.
+
+## Local Variables:
+## abbrev-mode:t
+## End:
